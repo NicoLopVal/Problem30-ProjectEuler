b16797251a85625c93c54a505a8f8e97d8b0d029
nikil <nikil@DESKTOP-73MHGNO>
2022-01-11 06:32:48 -0500

    Add project files.

---
+++ Problem30-ProjectEuler/Program.cs
@@ -0,0 +1,30 @@
+﻿
+List<double> selectedValues = new List<double>();
+
+double topLimit = 200000;
+
+for(double i = 2; i <= topLimit; i++)
+{
+    if(i == GetSumDigitsPow(i))
+        selectedValues.Add(i);
+}
+
+double totalSum = 0;
+foreach(double value in selectedValues)
+{
+    totalSum = totalSum + value;
+}
+
+Console.WriteLine("The sum of all the numbers that can be written as the sum of fifth powers of their digits is: " + totalSum);
+
+double GetSumDigitsPow(double value)
+{
+    string strValue = value.ToString();
+    double sum = 0;
+
+    foreach(char c in strValue)
+    {
+        sum = sum + Math.Pow(Convert.ToDouble(c.ToString()), 5);
+    }
+    return sum;
+}
